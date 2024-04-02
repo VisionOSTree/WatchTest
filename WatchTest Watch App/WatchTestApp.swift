b6fe9aca1a6b7e78c484d462c5f3611d9439d1de
Bocchi <oce.bocchi@gmail.com>
2024-04-02 11:24:57 +1100

    Initial Commit

---
+++ WatchTest Watch App/WatchTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchTestApp.swift
+//  WatchTest Watch App
+//
+//  Created by Daniel Braithwaite on 2/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct WatchTest_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
