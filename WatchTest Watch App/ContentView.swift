4efa02ca0074db355f780b1ca3f06958734a85ad
Bocchi <oce.bocchi@gmail.com>
2024-04-02 14:36:40 +1100

    initial commit

--- WatchTest Watch App/ContentView.swift
+++ WatchTest Watch App/ContentView.swift
@@ -6,16 +6,36 @@
 //
 
 import SwiftUI
+import HealthKit
 
 struct ContentView: View {
+    @State private var heartRate: Double = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Your heart rate is: \(heartRate)")
+            Button(action: self.getHeartRate, label: {
+                Text("Get heart rate")
+            })
         }
         .padding()
+    }
+    
+    private func getHeartRate(){
+        let healthStore = HKHealthStore()
+        
+        let heartRateType = HKQuantityType.quantityType(forIdentifier: .heartRate)!
+        
+        let date = Date();
+        
+        let predicate = HKQuery.predicateForSamples(withStart: date.addingTimeInterval(-60), end: date, options: .strictEndDate)
+        
+        let query = HKStatisticsQuery(quantityType: heartRateType, quantitySamplePredicate: predicate, options: .discreteAverage) { _, result, _ in
+            guard let result = result, let quantity = result.averageQuantity() else {
+                return
+            }
+            self.heartRate = quantity.doubleValue(for: HKUnit(from: "count/min"))
+            }
+        healthStore.execute(query)
     }
 }
 
